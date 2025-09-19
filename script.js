5e59f93aeee36f57abb4c0ad82137c3de45e2c57
Georgia Twumasi <twumasigm.vcu.edu>
2025-09-18 20:52:38 -0400

    commit script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
 // For each sound, create a button and a class 'btn', update the text, append to DOM
-sounds.forEach(sound) => {
+sounds.forEach((sound) => {
     const btn = document.createElement('button');
 
     // add a class of 'btn' to the button element
@@ -10,6 +10,11 @@ sounds.forEach(sound) => {
 
     //set the button label text to be the same at the string value in the sounds array
     btn.innerText = sound;
+
+    // Add an event listener to this button
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play();
+    });
 
     // Add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
